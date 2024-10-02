197dd869bad4515dfc6677ec97e9a79b482f9a4e
Appendix Radiant <naebcik@gmail.com>
2024-10-02 14:16:04 +0300

    Add project files.

---
+++ l_4/Program_1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+class MyMatrix
+{
+    private int[,] matrix;
+    private int n, m;
+
+    public MyMatrix(int n, int m, int min_val, int max_val)
+    {
+        this.n = n;
+        this.m = m;
+        this.matrix = new int[n, m];
+        Random rand = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++) { matrix[i, j] = rand.Next(min_val, max_val + 1); }
+        }
+    }
+    
+    public int this[int i, int j]
+    {
+        get { return matrix[i, j]; }
+        set { matrix[i, j] = value; }
+    }
+
+    public static MyMatrix operator +(MyMatrix a, MyMatrix b)
+    {
+        if (a.n != b.n || a.m != b.m) { throw new Exception("Matrices have different dimensions"); }
+        MyMatrix result = new MyMatrix(a.n, a.m, 0, 0);
+        for (int i = 0; i < a.n; i++)
+        {
+            for (int j = 0; j < a.m; j++) { result.matrix[i, j] = a.matrix[i, j] + b.matrix[i, j]; }
+        }
+        return result;
+    }
+
+    public static MyMatrix operator -(MyMatrix a, MyMatrix b)
+    {
+        if (a.n != b.n || a.m != b.m) { throw new Exception("Matrices have different dimensions"); }
+        MyMatrix result = new MyMatrix(a.n, a.m, 0, 0);
+        for (int i = 0; i < a.n; i++)
+        {
+            for (int j = 0; j < a.m; j++) { result.matrix[i, j] = a.matrix[i, j] - b.matrix[i, j]; }
+        }
+        return result;
+    }
+
+    public static MyMatrix operator *(MyMatrix a, MyMatrix b)
+    {
+        if (a.n != b.m) { throw new Exception("Matrices dimensions are not suitable for multiplication"); }
+        MyMatrix result = new MyMatrix(a.n, b.m, 0, 0);
+        for (int i = 0; i < a.n; i++)
+        {
+            for (int j = 0; j < b.m; j++)
+            {
+                for (int k = 0; k < a.m; k++) { result.matrix[i, j] += a.matrix[i, k] * b.matrix[k, j]; }
+            }
+        }
+        return result;
+    }
+
+    public static MyMatrix operator *(MyMatrix a, int scalar)
+    {
+        MyMatrix result = new MyMatrix(a.n, a.m, 0, 0);
+        for (int i = 0; i < a.n; i++)
+        {
+            for (int j = 0; j < a.m; j++) { result.matrix[i, j] = a.matrix[i, j] * scalar; }
+        }
+        return result;
+    }
+
+    public static MyMatrix operator /(MyMatrix a, int scalar)
+    {
+        MyMatrix result = new MyMatrix(a.n, a.m, 0, 0);
+        for (int i = 0; i < a.n; i++)
+        {
+            for (int j = 0; j < a.m; j++) { result.matrix[i, j] = a.matrix[i, j] / scalar; }
+        }
+        return result;
+    }
+
+    public void Print()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++) { Console.Write($"{matrix[i, j]} "); }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int n, m, min_val, max_val;
+
+        Console.WriteLine("Enter the number of rows:");
+        n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the number of columns:");
+        m = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the minimum value for random numbers:");
+        min_val = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the maximum value for random numbers:");
+        max_val = int.Parse(Console.ReadLine());
+
+        MyMatrix matrixA = new MyMatrix(n, m, min_val, max_val);
+        MyMatrix matrixB = new MyMatrix(n, m, min_val, max_val);
+
+        Console.WriteLine("Matrix A:");
+        matrixA.Print();
+        Console.WriteLine("Matrix B:");
+        matrixB.Print();
+
+        Console.WriteLine("Sum of matrices:");
+        (matrixA + matrixB).Print();
+
+        Console.WriteLine("Difference of matrices:");
+        (matrixA - matrixB).Print();
+
+        Console.WriteLine("Product of matrices:");
+        (matrixA * matrixB).Print();
+
+        int scalar = 5;
+        Console.WriteLine($"Matrix A multiplied by {scalar}:");
+        (matrixA * scalar).Print();
+
+        int divisor = 2;
+        Console.WriteLine($"Matrix A divided by {divisor}:");
+        (matrixA / divisor).Print();
+    }
+}
---
+++ l_4_2/Program_2.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+
+public class Car
+{
+    public string Name { get; set; }
+    public int ProductionYear { get; set; }
+    public int MaxSpeed { get; set; }
+}
+
+public class CarComparer : IComparer<Car>
+{
+    private string sortBy;
+    public CarComparer(string sortBy) { this.sortBy = sortBy; }
+    public int Compare(Car x, Car y)
+    {
+        switch (sortBy)
+        {
+            case "Name":
+                return string.Compare(x.Name, y.Name);
+            case "ProductionYear":
+                return x.ProductionYear.CompareTo(y.ProductionYear);
+            case "MaxSpeed":
+                return x.MaxSpeed.CompareTo(y.MaxSpeed);
+            default:
+                throw new ArgumentException("Invalid sort option");
+        }
+    }
+}
+
+public class CarCatalog : IEnumerable<Car>
+{
+    private Car[] cars;
+    public CarCatalog(Car[] cars) { this.cars = cars; }
+
+    // Прямой проход с первого элемента до последнего
+    public IEnumerator<Car> GetEnumerator()
+    {
+        for (int i = 0; i < cars.Length; i++) { yield return cars[i]; }
+    }
+
+    // Обратный проход от последнего к первому
+    public IEnumerable<Car> GetReverseEnumerator()
+    {
+        for (int i = cars.Length - 1; i >= 0; i--) { yield return cars[i]; }
+    }
+
+    // Проход по элементам массива с фильтром по году выпуска
+    public IEnumerable<Car> GetByProductionYearEnumerator(int year)
+    {
+        foreach (Car car in cars)
+        {
+            if (car.ProductionYear == year) { yield return car; }
+        }
+    }
+
+    // Проход по элементам массива с фильтром по максимальной скорости
+    public IEnumerable<Car> GetByMaxSpeedEnumerator(int speed)
+    {
+        foreach (Car car in cars)
+        {
+            if (car.MaxSpeed == speed) { yield return car; }
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car[] cars = new Car[]
+        {
+            new Car { Name = "Toyota", ProductionYear = 2019, MaxSpeed = 180 },
+            new Car { Name = "BMW", ProductionYear = 2018, MaxSpeed = 200 },
+            new Car { Name = "Maserati", ProductionYear = 2020, MaxSpeed = 300 }
+        };
+
+        // Сортировка по названию
+        Array.Sort(cars, new CarComparer("Name"));
+        Console.WriteLine("Sort by name:");
+        foreach (var car in cars) { Console.WriteLine($"{car.Name}, {car.ProductionYear}, {car.MaxSpeed}"); }
+
+        // Сортировка по году выпуска
+        Array.Sort(cars, new CarComparer("ProductionYear"));
+        Console.WriteLine("\nSort by year of production:");
+        foreach (var car in cars) { Console.WriteLine($"{car.Name}, {car.ProductionYear}, {car.MaxSpeed}"); }
+
+        // Сортировка по максимальной скорости
+        Array.Sort(cars, new CarComparer("MaxSpeed"));
+        Console.WriteLine("\nSort by max speed:");
+        foreach (var car in cars) { Console.WriteLine($"{car.Name}, {car.ProductionYear}, {car.MaxSpeed}"); }
+
+        Console.WriteLine();
+
+        CarCatalog catalog = new CarCatalog(cars);
+
+        foreach (Car car in catalog) { Console.WriteLine(car.Name); }
+        Console.WriteLine();
+
+        foreach (Car car in catalog.GetReverseEnumerator()) { Console.WriteLine(car.Name); }
+        Console.WriteLine();
+
+        foreach (Car car in catalog.GetByProductionYearEnumerator(2020)) { Console.WriteLine(car.Name); }
+        Console.WriteLine();
+
+        foreach (Car car in catalog.GetByMaxSpeedEnumerator(200)) { Console.WriteLine(car.Name); }
+        Console.WriteLine();
+    }
+}
